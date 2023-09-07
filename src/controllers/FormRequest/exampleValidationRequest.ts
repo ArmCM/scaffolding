0bea0be90e964f3ae5f30c1f7334885641651e1b
Armando Monreal <acalderon.root@gmail.com>
2023-09-07 16:28:06 -0600

    Remove comments

--- src/controllers/FormRequest/exampleValidationRequest.ts
+++ src/controllers/FormRequest/exampleValidationRequest.ts
@@ -1,6 +1,5 @@
 import { body } from 'express-validator';
 
-// This function returns an array of validation middleware for the 'createTask' request
 export function exampleValidationRequest() {
     return [
         body('title').notEmpty().withMessage('Title is required').isString().withMessage('Title must be a string'),

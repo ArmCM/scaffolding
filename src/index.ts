b93650416489bea3a4aed3509f4afe6302d58df5
Armando Monreal <acalderon.root@gmail.com>
2023-09-08 14:05:19 -0600

    Add function process to indicate when app is close

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,13 @@ import dotenv from 'dotenv';
 import PrismaSingleton from "./database/prismaClient";
 
 dotenv.config();
-
 const prisma = PrismaSingleton.getInstance();
 
-new App(3000, prisma);
+const app = new App(3000, prisma);
+
+process.on('SIGINT', () => {
+    app.stop().then(() => {
+        console.log('⚠️- App stopped gracefully');
+        process.exit(0);
+    });
+});
